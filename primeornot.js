f170f10e8bbe45c75d11070c9ad4175c0caea670
hazlind <hazlindsouza47>
2018-07-09 11:21:02 +0530

    2 files in branch collab

--- primeornot.js
+++ primeornot.js
@@ -13,4 +13,17 @@ for(let i=0;i<n;i++)
         console.log('prime');
     }
 }
-//subtract wthout using minus
+//division without modulus(%)
+var n=9;
+var div=4;
+var a,b,r;
+a=n/div;
+console.log(a);
+b=Math.floor(a);
+console.log(b);
+r=a-b;
+r=Math.round(r*div);
+console.log('remained is',r);
+
+
+//
